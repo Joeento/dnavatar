611bb3537c00c04ac9cf760ac1fec0c231741e78
Eric Kudler <joeento@gmail.com>
2018-08-12 18:23:48 -0400

    Some style work to introduce the control panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Image, Panel, Grid, Row, Col} from 'react-bootstrap';
 import logo from './logo.svg';
+import libmoji from 'libmoji';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-class NavigationBar extends React.Component {
+class NavigationBar extends Component {
     render() {
         return (
             <Navbar>
@@ -33,11 +35,54 @@ class NavigationBar extends React.Component {
         );
     }
 }
+
+class PreviewPanel extends Component {
+    render() {
+        return (
+            <Panel>
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">Preview</Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <Image width="300" src="https://preview.bitmoji.com/avatar-builder-v3/preview/body?scale=3&gender=2&style=1&rotation=0&brow=109&cheek_details=241&ear=246&earring=511&eye=147&eyelash=-1&eye_details=634&face_lines=250&glasses=561&hair=321&hat=27&jaw=285&mouth=196&nose=625&pupil=226&blush_tone=-1&brow_tone=3415562&eyeshadow_tone=12098752&hair_tone=8637550&lipstick_tone=2565927&pupil_tone=6064564&skin_tone=12434877&body=2&breast=1&face_proportion=1&outfit=889202" />
+                </Panel.Body>
+            </Panel>
+        )
+    }
+}
+
+class ControlPanel extends Component {
+    render() {
+        return (
+            <Panel>
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">Options</Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <Image width="500" src="https://preview.bitmoji.com/avatar-builder-v3/preview/body?scale=3&gender=2&style=1&rotation=0&brow=109&cheek_details=241&ear=246&earring=511&eye=147&eyelash=-1&eye_details=634&face_lines=250&glasses=561&hair=321&hat=27&jaw=285&mouth=196&nose=625&pupil=226&blush_tone=-1&brow_tone=3415562&eyeshadow_tone=12098752&hair_tone=8637550&lipstick_tone=2565927&pupil_tone=6064564&skin_tone=12434877&body=2&breast=1&face_proportion=1&outfit=889202" />
+                </Panel.Body>
+            </Panel>
+        )
+    }
+}
+
 class App extends Component {
     render() {
         return (
             <div>
                 <NavigationBar />
+                <div className="container">
+                    <Grid>
+                        <Row>
+                            <Col md={4}>
+                                <PreviewPanel />
+                            </Col>
+                            <Col md={8}>
+                                <ControlPanel />
+                            </Col>
+                        </Row>
+                    </Grid>
+                </div>
             </div>
         );
     }
