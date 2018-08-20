01cf497a251a5902c9f5913eb74030b988deec7e
Eric Kudler <joeento@gmail.com>
2018-08-20 12:18:38 -0400

    Adds authentication endpoints to server

--- server.js
+++ server.js
@@ -3,12 +3,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const session = require('express-session');
 const port = process.env.PORT || 5000;
+
+const genomeLink = require('genomelink-node');
+
+app.use(session({
+	secret: 'YOURSECRET',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		maxAge: 30 * 60 * 1000
+	}
+}));
 
 // API calls
 app.get('/api/hello', (req, res) => {
 	res.send({ express: 'Hello From Express' });
 });
+
+app.get('/api/auth', (req, res) => {
+	const scope = 'report:eye-color report:beard-thickness report:morning-person';
+	const authorizeUrl = genomeLink.OAuth.authorizeUrl({ scope: scope });
+	res.send({
+		authorize_url: authorizeUrl,
+		is_authed: req.session.oauthToken ? true : false
+	});
+});
+
+app.get('/api/code', async (req, res) => {
+	req.session.oauthToken = await genomeLink.OAuth.token({ requestUrl: req.url });
+	res.send({
+		is_authed: req.session.oauthToken ? true : false
+	});
+});
+
 
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
